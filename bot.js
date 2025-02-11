10680254b9966efb99671ae8c6a6a06ba9af97da
Harry Xu <harryxu2626@gmail.com>
2025-02-11 09:15:50 -0500

    added ratings update into setInterval function

--- bot.js
+++ bot.js
@@ -47,6 +47,8 @@ const positionUpdate = async () => {
 
     const position = data.onlinePosition;
 
+    dataUpdate(data);
+
     const serverGroupsById = await teamspeak.serverGroupsByClientId(clientDbid.cldbid);
 
     if (position) {
@@ -118,9 +120,95 @@ const removePosition = async (client) => {
   }
 }
 
+const dataUpdate = async (data) => {
+  const controllerStatus = data.controllerStatus === "HOME" ? 1 : 0;
+  const rating = data.rating;
+
+  // console.log(client.nickname,controllerStatus,rating,position,clientDbid);
+
+  switch (controllerStatus) {
+    case 0:
+      if (
+        !serverGroupsById.some((item) => item.name === "Visitor") &&
+        rating
+      ) {
+        client.addGroups(await teamspeak.getServerGroupByName("Visitor"));
+      }
+      break;
+    case 1:
+      if (!serverGroupsById.some((item) => item.name === "Member")) {
+        client.addGroups(await teamspeak.getServerGroupByName("Member"));
+      }
+      break;
+  }
+
+  switch (rating) {
+    case 0:
+      client.kickFromServer("Suspended members do not have access to the TeamSpeak");
+    case 1:
+      if (!serverGroupsById.some((item) => item.name === "Observer")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Observer"));
+      }
+      break;
+    case 2:
+      if (!serverGroupsById.some((item) => item.name === "Tower Trainee")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Tower Trainee"));
+      }
+      break;
+    case 3:
+      if (!serverGroupsById.some((item) => item.name === "Tower Controller")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Tower Controller"));
+      }
+      break;
+    case 4:
+      if (!serverGroupsById.some((item) => item.name === "Radar Controller")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Radar Controller"));
+      }
+      break;
+    case 5:
+      if (!serverGroupsById.some((item) => item.name === "Controller")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Controller"));
+      }
+      break;
+    case 7:
+      if (!serverGroupsById.some((item) => item.name === "Senior Controller")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Senior Controller"));
+      }
+      break;
+    case 8:
+      if (!serverGroupsById.some((item) => item.name === "Instructor")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Instructor"));
+      }
+      break;
+    case 10:
+      if (!serverGroupsById.some((item) => item.name === "Senior Instructor")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("Senior Instructor"));
+      }
+      break;
+    case 11:
+      if (!serverGroupsById.some((item) => item.name === "VATSIM Supervisor")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("VATSIM Supervisor"));
+      }
+      break;
+    case 12:
+      if (!serverGroupsById.some((item) => item.name === "VATSIM Admin")) {
+        clearRatings(client)
+        client.addGroups(await teamspeak.getServerGroupByName("VATSIM Admin"));
+      }
+      break;
+  }
+}
 
 teamspeak.on("ready", async () => {
-  positionUpdate();
   setInterval(positionUpdate,60*1000);
 });
 
@@ -152,91 +240,7 @@ teamspeak.on("clientconnect", async (connected) => {
       return;
     }
   
-    const controllerStatus = data.controllerStatus === "HOME" ? 1 : 0;
-    const rating = data.rating;
-  
-    // console.log(client.nickname,controllerStatus,rating,position,clientDbid);
-  
-    switch (controllerStatus) {
-      case 0:
-        if (
-          !serverGroupsById.some((item) => item.name === "Visitor") &&
-          rating
-        ) {
-          client.addGroups(await teamspeak.getServerGroupByName("Visitor"));
-        }
-        break;
-      case 1:
-        if (!serverGroupsById.some((item) => item.name === "Member")) {
-          client.addGroups(await teamspeak.getServerGroupByName("Member"));
-        }
-        break;
-    }
-  
-    switch (rating) {
-      case 0:
-        client.kickFromServer("Suspended members do not have access to the TeamSpeak");
-      case 1:
-        if (!serverGroupsById.some((item) => item.name === "Observer")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Observer"));
-        }
-        break;
-      case 2:
-        if (!serverGroupsById.some((item) => item.name === "Tower Trainee")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Tower Trainee"));
-        }
-        break;
-      case 3:
-        if (!serverGroupsById.some((item) => item.name === "Tower Controller")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Tower Controller"));
-        }
-        break;
-      case 4:
-        if (!serverGroupsById.some((item) => item.name === "Radar Controller")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Radar Controller"));
-        }
-        break;
-      case 5:
-        if (!serverGroupsById.some((item) => item.name === "Controller")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Controller"));
-        }
-        break;
-      case 7:
-        if (!serverGroupsById.some((item) => item.name === "Senior Controller")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Senior Controller"));
-        }
-        break;
-      case 8:
-        if (!serverGroupsById.some((item) => item.name === "Instructor")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Instructor"));
-        }
-        break;
-      case 10:
-        if (!serverGroupsById.some((item) => item.name === "Senior Instructor")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("Senior Instructor"));
-        }
-        break;
-      case 11:
-        if (!serverGroupsById.some((item) => item.name === "VATSIM Supervisor")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("VATSIM Supervisor"));
-        }
-        break;
-      case 12:
-        if (!serverGroupsById.some((item) => item.name === "VATSIM Admin")) {
-          clearRatings(client)
-          client.addGroups(await teamspeak.getServerGroupByName("VATSIM Admin"));
-        }
-        break;
-    }
+    dataUpdate(data);
 })
 
 teamspeak.on("clientdisconnect", async (connected) => {
